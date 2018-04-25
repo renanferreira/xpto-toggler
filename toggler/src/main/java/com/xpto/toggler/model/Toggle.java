f280f26447e301ef81baac1030702a1f13a9a8cd
Renan Ferreira <renan@wonka.lan>
2018-04-25 10:11:48 -0300

    Model and Resource classes

--- toggler/src/main/java/com/xpto/toggler/model/Toggle.java
+++ toggler/src/main/java/com/xpto/toggler/model/Toggle.java
@@ -8,4 +8,42 @@ public class Toggle {
 	private int priority;
 	private boolean override;
 	private List<String> allowedServices;
+	private String username;
+	
+	public String getToogleName() {
+		return toogleName;
+	}
+	public void setToogleName(String toogleName) {
+		this.toogleName = toogleName;
+	}
+	public boolean isToogleValue() {
+		return toogleValue;
+	}
+	public void setToogleValue(boolean toogleValue) {
+		this.toogleValue = toogleValue;
+	}
+	public int getPriority() {
+		return priority;
+	}
+	public void setPriority(int priority) {
+		this.priority = priority;
+	}
+	public boolean isOverride() {
+		return override;
+	}
+	public void setOverride(boolean override) {
+		this.override = override;
+	}
+	public List<String> getAllowedServices() {
+		return allowedServices;
+	}
+	public void setAllowedServices(List<String> allowedServices) {
+		this.allowedServices = allowedServices;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
 }
